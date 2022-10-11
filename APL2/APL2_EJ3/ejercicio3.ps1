c89f02fd701af06eed4a9ce6e46ad7f9242263b6
Julian <julian.leonel3004@gmaio.com>
2022-10-11 09:32:26 -0300

    esquema sin acciones

--- APL2/APL2_EJ3/ejercicio3.ps1
+++ APL2/APL2_EJ3/ejercicio3.ps1
@@ -43,30 +43,13 @@ Param(
 
 #VALIDAR
 
-$c = Resolve-Path $c 
-#CREAR DEMONIO
-# find the path to the desktop folder:
+$acciones = @($a -split ",")
 
-# specify the path to the folder you want to monitor:
-$Path = $c
+#-----------------------------------------
 
-# specify which files you want to monitor
-$FileFilter = '*'  
-
-# specify whether you want to monitor subfolders as well:
-$IncludeSubfolders = $true
-
-# specify the file or folder properties you want to monitor:
-$AttributeFilter = [IO.NotifyFilters]::FileName, [IO.NotifyFilters]::LastWrite 
-
-# specify the type of changes you want to monitor:
-$ChangeTypes = [System.IO.WatcherChangeTypes]::Created, [System.IO.WatcherChangeTypes]::Deleted, [System.IO.WatcherChangeTypes]::Changed,  [System.IO.WatcherChangeTypes]::Renamed
-
-# specify the maximum time (in milliseconds) you want to wait for changes:
-$Timeout = 1000
 
 # define a function that gets called for every change:
-function Invoke-SomeAction
+function ejercutar_accion
 {
   param
   (
@@ -75,8 +58,24 @@ function Invoke-SomeAction
     $ChangeInformation
   )
   
-  Write-Warning 'Change detected:'
-  $ChangeInformation | Out-String | Write-Host -ForegroundColor DarkYellow
+  foreach ($accion in $acciones)  {
+    if ($accion -eq "compilar") {
+        "compilar"
+    }
+    if ($accion -eq "publicar") {
+       "publicar"
+    }
+    if($accion -eq "listar"){
+        $ChangeInformation | Out-String | Write-Host -ForegroundColor DarkYellow
+    }
+    if($accion -eq "peso"){
+     # $name = $ChangeInformation::Size
+     # Write-Host $name
+
+    }
+}
+
+ 
 
 
 }
@@ -85,35 +84,62 @@ function Invoke-SomeAction
 # filesystemwatcher once the loop is aborted
 # by pressing CTRL+C
 
-try
-{
-  Write-Warning "FileSystemWatcher is monitoring $Path"
-  
-  # create a filesystemwatcher object
-  $watcher = New-Object -TypeName IO.FileSystemWatcher -ArgumentList $Path, $FileFilter -Property @{
-    IncludeSubdirectories = $IncludeSubfolders
-    NotifyFilter = $AttributeFilter
-  }
+function monitoreo {
 
-  # start monitoring manually in a loop:
-  do
+  $c = Resolve-Path $c 
+
+  # specify the path to the folder you want to monitor:
+  $Path = $c
+
+  # specify which files you want to monitor
+  $FileFilter = '*'  
+
+  # specify whether you want to monitor subfolders as well:
+  $IncludeSubfolders = $true
+
+  # specify the file or folder properties you want to monitor:
+  $AttributeFilter = [IO.NotifyFilters]::FileName, [IO.NotifyFilters]::LastWrite, [IO.NotifyFilters]::Size
+
+  # specify the type of changes you want to monitor:
+  $ChangeTypes = [System.IO.WatcherChangeTypes]::Created, [System.IO.WatcherChangeTypes]::Deleted, [System.IO.WatcherChangeTypes]::Changed,  [System.IO.WatcherChangeTypes]::Renamed
+
+  # specify the maximum time (in milliseconds) you want to wait for changes:
+  $Timeout = 1000
+
+
+  try
   {
-    # wait for changes for the specified timeout
-    # IMPORTANT: while the watcher is active, PowerShell cannot be stopped
-    # so it is recommended to use a timeout of 1000ms and repeat the
-    # monitoring in a loop. This way, you have the chance to abort the
-    # script every second.
-    $result = $watcher.WaitForChanged($ChangeTypes, $Timeout)
-    # if there was a timeout, continue monitoring:
-    if ($result.TimedOut) { continue }
-    
-    Invoke-SomeAction -Change $result
-    # the loop runs forever until you hit CTRL+C    
-  } while ($true)
+    Write-Warning "Se inicio el monitoreo de $Path"
+
+    # create a filesystemwatcher object
+    $watcher = New-Object -TypeName IO.FileSystemWatcher -ArgumentList $Path, $FileFilter -Property @{
+      IncludeSubdirectories = $IncludeSubfolders
+      NotifyFilter = $AttributeFilter
+    }
+
+    # start monitoring manually in a loop:
+    do
+    {
+      # wait for changes for the specified timeout
+      # IMPORTANT: while the watcher is active, PowerShell cannot be stopped
+      # so it is recommended to use a timeout of 1000ms and repeat the
+      # monitoring in a loop. This way, you have the chance to abort the
+      # script every second.
+      $result = $watcher.WaitForChanged($ChangeTypes, $Timeout)
+      # if there was a timeout, continue monitoring:
+      if ($result.TimedOut) { continue }
+
+      ejercutar_accion -Change $result
+      # the loop runs forever until you hit CTRL+C    
+    } while ($true)
+  }
+  finally
+  {
+    # release the watcher and free its memory:
+    $watcher.Dispose()
+    Write-Warning 'Fin del monitoreo'
+  }
 }
-finally
-{
-  # release the watcher and free its memory:
-  $watcher.Dispose()
-  Write-Warning 'FileSystemWatcher removed.'
-}
+
+
+monitoreo
